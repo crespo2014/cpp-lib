b1c073eab8872b950214de772a354383b8a83190
Lester <lester.crespo.es@gmail.com>
2015-01-24 21:14:05 +0000

    File reimplement as fd

--- posix/File.cpp
+++ posix/File.cpp
@@ -4,30 +4,33 @@
  *  Created on: 24 Jan 2015
  *      Author: lester
  */
-
+#include <system_error>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 #include "File.h"
 
 namespace POSIX {
 
-File::File()
+File::File(const char* name,int flag)
 {
-  // TODO Auto-generated constructor stub
-
+    file_ = ::open(name,flag);
+    if (file_ == -1) throw std::system_error(errno,std::system_category());
+    status_ |= status_e::ok;
 }
 
-File::File(const char* name, const char* mode)
+File::File(const char* name, int flag,const std::nothrow_t& t)
 {
-}
-
-File::File(const char* name, const char* mode,const std::nothrow_t& t)
-{
-  file_ = fopen(name,mode);
-  if (file_ == nullptr)
+  file_ = ::open(name,flag);
+  if (file_ == -1)
     status_ |= status_e::fail;
 }
 
-File::File(File&& f)
+File::File(File&& f) : file_(f.file_),status_(f.status_)
 {
+    f.file_ = -1;
 }
 
 File& File::operator =(const File&& f)
@@ -37,7 +40,9 @@ File& File::operator =(const File&& f)
 
 File::~File()
 {
-
+    if (file_ != -1)
+        ::close(file_);
+        //close(std::nothrow);
 }
 
 bool File::good()
@@ -50,12 +55,21 @@ bool File::eof()
   return (status_ & status_e::eof_);
 }
 
+//bool File::close(const std::nothrow_t& t)
+//{
+//    fclose(file_) != 0)
+//}
+
+void File::close()
+{
+    if (::close(file_) != 0)
+        throw std::system_error(errno,std::system_category());
+    file_ = -1;
+}
+
+int File::ioctl(unsigned long request, void* arg)
+{
+    return ::ioctl(file_,request,arg);
+}
+
 } /* namespace POSIX */
-
-bool POSIX::File::close(const std::nothrow_t& t)
-{
-}
-
-void POSIX::File::close()
-{
-}
--- posix/File.h
+++ posix/File.h
@@ -17,20 +17,20 @@ class File
 {
   File& operator=(const File&)=delete;
   File(const File&)= delete;
-  bool close(const std::nothrow_t& t);
+  //bool close(const std::nothrow_t& t);
 public:
-  File(const char* name,const char* mode);
-  File(const char* name,const char* mode,const std::nothrow_t& t);
+  File(const char* name,int flag);
+  File(const char* name,int flag,const std::nothrow_t& t);
   File(File&&);
   File& operator=(const File&&);
-  File();
   virtual ~File();
   bool good();
   bool eof();
   void close();
+  int ioctl(unsigned long request, void* arg);
 protected:
 private:
-  FILE * file_ = nullptr;
+  int file_ = -1;
   unsigned status_ = status_e::closed;
   enum status_e { ok,fail,eof_,closed };
 };
